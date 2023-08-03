de9eecd8f6bf25d0f20db91645302e126ee074c7
Anh kiet <anhkiethr300@gmail.com>
2023-08-03 18:44:49 +0700

    first commit

--- Lab4-1.js
+++ Lab4-1.js
@@ -1,31 +1,20 @@
-var lyricElemment = document.getElementById('lyric');
-lyrics = [
-    'Đoàn quân Việt Nam đi chung lòng cứu quốc, ',
-    'bước chân dồn vang trên đường gập ghềnh xa, ',
-    'cờ in máu chiến thắng mang hồn nước, ',
-    'súng ngoài xa chen khúc quân hành ca, ',
-    'đường vinh quang xây xác quân thù, ',
-    'hắng gian lao cùng nhau lập chiến khu, ',
-    'vì nhân dân chiến đấu không ngừng, ',
-    'tiến mau ra sa trường. ',
-    'Tiến lên, ',
-    'cùng tiến lên, ',
-    'nước non Việt Nam ta vững bền.'
-]
+const user1 = ['1m7', '65kg']
+const user2 = ['1m8', '70kg']
+const user3 = ['1m5', '45kg']
+
+localStorage.setItem('Phước', JSON.stringify(user1))
+localStorage.setItem('An', JSON.stringify(user2))
+localStorage.setItem('Linh', JSON.stringify(user3))
+localStorage.removeItem('Hà')
 
 
-
-let i = 0;
-var lyric = ''
-
-let time = setInterval(function ABC() {
-    lyric = lyric + lyrics[i]
-
-    var lyricsHTML = `<p id="lyric">${lyric}</p>`;
-    lyricElemment.innerHTML = lyricsHTML;
-    i++
-}, 1000)
-
-function clearAlert() {
-    clearTimeout(time);
+function change_name() {
+    localStorage.setItem('Hà', JSON.stringify(user1))
+    localStorage.removeItem('Phước')
+}
+function delete_Phuoc() {
+    localStorage.removeItem('Phước')
+}
+function clear_localStorage() {
+    localStorage.clear()
 }
